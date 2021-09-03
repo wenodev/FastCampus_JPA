3ea95ee6cb7eae32817adae0c19cba64bd2963cf
wenodev <logicalstark1105@gmail.com>
2021-09-03 09:57:35 +0900

    feat: OneToMany 단방향을 위한 Board, Comment 엔티티 생성하라

---
+++ src/main/java/com/example/FastJpa/apractice/onetomanyunidirectional/Comment.java
@@ -0,0 +1,31 @@
+package com.example.FastJpa.apractice.onetomanyunidirectional;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@ToString
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String content;
+}
