c762c8ae0032cab043f666bca002dc25222ae228
wenodev <logicalstark1105@gmail.com>
2021-09-06 17:03:19 +0900

    create 멧메소드 생성하라

--- src/main/java/com/example/FastJpa/service/UserService.java
+++ src/main/java/com/example/FastJpa/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.FastJpa.service;
 
+import com.example.FastJpa.domain.User;
 import com.example.FastJpa.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,5 +12,9 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User create(User name) {
+        return userRepository.save(name);
     }
 }
