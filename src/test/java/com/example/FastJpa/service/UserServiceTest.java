b9a0f972b0cc3eee92d9a0a6c82e943c6e54085f
wenodev <logicalstark1105@gmail.com>
2021-09-06 17:58:23 +0900

    feat: UserHistory 테스트 추가하라

--- src/test/java/com/example/FastJpa/service/UserServiceTest.java
+++ src/test/java/com/example/FastJpa/service/UserServiceTest.java
@@ -1,11 +1,12 @@
 package com.example.FastJpa.service;
 
+import com.example.FastJpa.domain.Address;
+import com.example.FastJpa.domain.Gender;
 import com.example.FastJpa.domain.User;
+import com.example.FastJpa.domain.UserHistory;
+import com.example.FastJpa.repository.UserHistoryRepository;
 import com.example.FastJpa.repository.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -15,24 +16,49 @@ import static org.mockito.Mockito.mock;
 class UserServiceTest {
 
     private UserRepository userRepository = mock(UserRepository.class);
-    private UserService userService = new UserService(userRepository);
+    private UserHistoryRepository userHistoryRepository = mock(UserHistoryRepository.class);
 
+    private UserService userService = new UserService(userRepository);
 
     @Test
     void save_user(){
+        Address homeAddress = Address.builder()
+                .city("home-city")
+                .build();
+
         User createUser = User.builder()
                 .id(1L)
                 .name("name")
+                .gender(Gender.MALE)
+                .homeAddress(homeAddress)
                 .build();
+
+
+        UserHistory createUserHistory = UserHistory.builder()
+                .id(1L)
+                .name("name")
+                .gender(Gender.MALE)
+                .user(createUser)
+                .build();
+
 
         //given
         given(userRepository.save(any(User.class))).willReturn(createUser);
+        given(userHistoryRepository.save(any(UserHistory.class))).willReturn(createUserHistory);
+
 
         //when
         createUser = userService.create(createUser);
 
         //then
         assertThat(createUser.getName()).isEqualTo("name");
+        assertThat(createUser.getGender()).isEqualTo(Gender.MALE);
+        assertThat(createUser.getHomeAddress().getCity()).isEqualTo("home-city");
+
+        assertThat(createUserHistory.getUser().getId()).isEqualTo(1L);
+        assertThat(createUserHistory.getGender()).isEqualTo(Gender.MALE);
     }
+
+
 
 }
