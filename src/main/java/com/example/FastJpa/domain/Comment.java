db51450ea0cf5d14a2d072b1958d570e2a3ba465
wenodev <logicalstark1105@gmail.com>
2021-09-06 13:42:03 +0900

    feat: Comment 생ã생성하라

--- src/main/java/com/example/FastJpa/domain/Comment.java
+++ src/main/java/com/example/FastJpa/domain/Comment.java
@@ -1,4 +1,46 @@
 package com.example.FastJpa.domain;
 
-public class Comment {
+import jdk.jfr.BooleanFlag;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Comment extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String comment;
+
+    @JoinColumn(name = "review_ID")
+    @ManyToOne
+    @ToString.Exclude
+    private Review review;
+
+    @Column(columnDefinition = "datetime(6) default now(6)")
+    private LocalDateTime commentedAt;
+
+    public void setReview(Review review) {
+        this.review = review;
+    }
+
 }
