387dbdedf2dc0a981b326bee10d7807f309b8611
wenodev <logicalstark1105@gmail.com>
2021-09-06 16:03:51 +0900

    feat: Book ãEntity 프로í퍼티 추가하라

--- src/main/java/com/example/FastJpa/domain/Book.java
+++ src/main/java/com/example/FastJpa/domain/Book.java
@@ -8,13 +8,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.Where;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import java.util.ArrayList;
@@ -45,7 +48,17 @@ public class Book extends BaseEntity{
     @OneToMany
     @JoinColumn(name = "book_id")
     @ToString.Exclude
+    private List<Review> reviews = new ArrayList<>();
+
+    @ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE })
+    @ToString.Exclude
+    private Publisher publisher;
+
+    @OneToMany
+    @JoinColumn(name = "book_id")
+    @ToString.Exclude
     private List<BookAndAuthor> bookAndAuthors = new ArrayList<>();
 
     private boolean deleted;
+
 }
