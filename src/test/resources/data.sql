d114125453eb117c78b6d2ae0e1e81a52160e3b3
wenodev <logicalstark1105@gmail.com>
2021-09-08 18:06:48 +0900

    fix: data.sql 파일 변경하라

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -13,10 +13,11 @@
 -- -- call next value for hibernate_sequence;
 -- insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (5, 'martin', 'martin@another.com', now(), now());
 
-insert into user (`id`, `name`, `email`) values (1, 'martin', 'martin@fastcampus.com');
-insert into user (`id`, `name`, `email`) values (2, 'dennis', 'dennis@fastcampus.com');
-insert into user (`id`, `name`, `email`) values (3, 'sophia', 'sophia@slowcampus.com');
-insert into user (`id`, `name`, `email`) values (4, 'james', 'james@slowcampus.com');
-insert into user (`id`, `name`, `email`) values (5, 'martin', 'martin@another.com');
+-- insert into user (`id`, `name`, `email`) values (1, 'martin', 'martin@fastcampus.com');
+-- insert into user (`id`, `name`, `email`) values (2, 'dennis', 'dennis@fastcampus.com');
+-- insert into user (`id`, `name`, `email`) values (3, 'sophia', 'sophia@slowcampus.com');
+-- insert into user (`id`, `name`, `email`) values (4, 'james', 'james@slowcampus.com');
+-- insert into user (`id`, `name`, `email`) values (5, 'martin', 'martin@another.com');
 
-insert into book (`id`, `name`) values (1, 'book-test-name');
+insert into user (`name`, `email`) values ('martin', 'martin@fastcampus.com');
+insert into book ( `name`, `deleted`) values ('book-test-name', false);
